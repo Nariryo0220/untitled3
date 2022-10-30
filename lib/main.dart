1245293399e79c8999380e0fcef4a311c0f9083b
narir <si19048@edu.aomori-u.ac.jp>
2022-10-30 17:09:24 +0900

    BabyName簡素版_自分のデータベースに置き換え 31~35 List<○○○> ○○○ = []; 44~48 document.foEach 53,54 Text 69~74 print

--- lib/main.dart
+++ lib/main.dart
@@ -30,23 +30,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<DocumentSnapshot> documentList =[];
   List<String> name = [];
-  List<int> votes = [];
+  List<double> ido = [];
+  List<double> keido = [];
+  List<GeoPoint> idokeido = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
+      appBar: AppBar(title: Text('Map')),
       body: FutureBuilder(
         future: initialize(),
         builder: (context, snapshot) {
           documentList.forEach((elem) {
             name.add(elem.get('name'));
-            votes.add(elem.get('votes'));
+            ido.add(elem.get('ido'));
+            keido.add(elem.get('keido'));
+            idokeido.add(elem.get('idokeido'));
+
           });
           return Column(
             children: <Widget> [
-              Text(name[0] + ':' + votes[0].toString()),
-              Text(name[1] + ':' + votes[1].toString()),
+              Text(name[0] + ':' + ido[0].toString() + ':' + keido[0].toString() + '###' + idokeido[0].latitude.toString() + ':' + idokeido[0].longitude.toString()),
+              Text(name[1] + ':' + ido[1].toString() + ':' + keido[1].toString() + '###' + idokeido[1].latitude.toString() + ':' + idokeido[1].longitude.toString()),
             ],
           );
         },
@@ -56,13 +61,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> initialize () async {
     final snapshot =
-      await FirebaseFirestore.instance.collection('baby').get();
+      await FirebaseFirestore.instance.collection('toire').get();
     documentList = snapshot.docs;
+    GeoPoint pos = const GeoPoint(0.0,0.0);
 
     print("##################################################### initialize()");
     documentList.forEach((elem) {
       print(elem.get('name'));
-      print(elem.get('votes'));
+      print(elem.get('ido'));
+      print(elem.get('keido'));
+      pos = elem.get('idokeido');
+      print(pos.latitude.toString() + ',' + pos.longitude.toString());
     });
     print("##################################################### initialize()");
   }
