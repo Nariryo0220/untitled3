3874b77f0e4fea704dbea5caa0b12f2188ded930
narir <si19048@edu.aomori-u.ac.jp>
2022-10-31 17:20:49 +0900

    Mapアプリに切り替え マーカーの表示（個別） 40~51 Mapに使う変数 54~81 initState _getUserLocation positionstream 96~120 Googlemap

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Baby Names',
+      title: 'Map',
       home: MyHomePage(),
     );
   }
@@ -28,13 +31,55 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List<DocumentSnapshot> documentList =[];
+  List<DocumentSnapshot> documentList = [];
   List<String> name = [];
   List<double> ido = [];
   List<double> keido = [];
   List<GeoPoint> idokeido = [];
 
+  Position? currentPosition;
+  late GoogleMapController _controller;
+  late StreamSubscription<Position> positionStream;
+  late LatLng _initialPosition;
+  late bool _loading;
+
+  GeoPoint pos = const GeoPoint(0.0, 0.0);
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 100,
+  );
+
   @override
+  void initState() {
+    super.initState();
+    _loading = true;
+    _getUserLocation();
+  }
+
+  void _getUserLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      _loading = false;
+      print(position);
+    });
+  }
+
+  //現在位置を更新し続ける
+  void positionstrem() {
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+          currentPosition = position;
+          print(position == null
+              ? 'Unknown'
+              : '${position.latitude.toString()}, ${position.longitude
+              .toString()}');
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Map')),
@@ -46,34 +91,59 @@ class _MyHomePageState extends State<MyHomePage> {
             ido.add(elem.get('ido'));
             keido.add(elem.get('keido'));
             idokeido.add(elem.get('idokeido'));
-
+            //pos = elem.get('idokeido');
           });
-          return Column(
-            children: <Widget> [
-              Text(name[0] + ':' + ido[0].toString() + ':' + keido[0].toString() + '###' + idokeido[0].latitude.toString() + ':' + idokeido[0].longitude.toString()),
-              Text(name[1] + ':' + ido[1].toString() + ':' + keido[1].toString() + '###' + idokeido[1].latitude.toString() + ':' + idokeido[1].longitude.toString()),
-            ],
+          return GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: _initialPosition,
+              zoom: 17,
+            ),
+            onMapCreated: (GoogleMapController controller) {
+              _controller = controller;
+            },
+            markers: [
+              Marker(
+                  markerId:(MarkerId(name[0])),
+                  //position: LatLng(ido[0],keido[0])
+                  position: LatLng(idokeido[0].latitude,idokeido[0].longitude)
+              ),
+              Marker(
+                  markerId:(MarkerId(name[1])),
+                  //position: LatLng(ido[1],keido[1])
+                  position: LatLng(idokeido[1].latitude,idokeido[1].longitude)
+              ),
+            ].toSet(),
+            onTap: (LatLng latLang) {
+              print('Clicked: $latLang');
+            },
+            myLocationEnabled: true,
           );
         },
       ),
     );
   }
 
-  Future<void> initialize () async {
+  Future<void> initialize() async {
     final snapshot =
-      await FirebaseFirestore.instance.collection('toire').get();
+    await FirebaseFirestore.instance.collection('toire').get();
     documentList = snapshot.docs;
-    GeoPoint pos = const GeoPoint(0.0,0.0);
+    GeoPoint pos_con = const GeoPoint(0.0, 0.0);
 
-    print("##################################################### initialize()");
-    documentList.forEach((elem) {
-      print(elem.get('name'));
-      print(elem.get('ido'));
-      print(elem.get('keido'));
-      pos = elem.get('idokeido');
-      print(pos.latitude.toString() + ',' + pos.longitude.toString());
-    });
-    print("##################################################### initialize()");
+    // LocationPermission permission = await Geolocator.checkPermission();
+    // if (permission == LocationPermission.denied) {
+    //   await Geolocator.requestPermission();
+
+      print(
+          "##################################################### initialize()");
+      documentList.forEach((elem) {
+        print(elem.get('name'));
+        print(elem.get('ido'));
+        print(elem.get('keido'));
+        pos_con = elem.get('idokeido');
+        print(pos_con.latitude.toString() + ',' + pos.longitude.toString());
+      });
+      print(
+          "##################################################### initialize()");
+    }
   }
-
-}
+//}
