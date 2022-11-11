897ad034092461add840bac02ab6b2595a558fc2
narir <si19048@edu.aomori-u.ac.jp>
2022-11-11 16:27:17 +0900

    Mapアプリ　initilize2の生成　marker→ontapで地図情報 38~45 List 105~112 documentList.forEach 115 return gm 130~138 print 145~196 initilize2 Googlemapとmarkerにontap

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ void main() async {
 }
 
 
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -32,16 +34,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   List<DocumentSnapshot> documentList = [];
-  List<String> name = [];
+
   List<double> ido = [];
   List<double> keido = [];
-  List<GeoPoint> idokeido = [];
+  List<String> kubun =[];
+  List<String> namae = [];
+  List<String> zyouhou1 = [];
+  List<String> zyouhou2 = [];
+  List<String> zyouhou3 = [];
+  List<String> zyuusyo = [];
+
 
   Position? currentPosition;
   late GoogleMapController _controller;
   late StreamSubscription<Position> positionStream;
   late LatLng _initialPosition;
   late bool _loading;
+  late GoogleMap gm;
 
   GeoPoint pos = const GeoPoint(0.0, 0.0);
 
@@ -53,10 +62,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+
+    // Future(() async {
+    //
+    //   }
+    // });
+
     _loading = true;
     _getUserLocation();
   }
 
+  //現在地を取得する
   void _getUserLocation() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
@@ -87,57 +103,106 @@ class _MyHomePageState extends State<MyHomePage> {
         future: initialize(),
         builder: (context, snapshot) {
           documentList.forEach((elem) {
-            name.add(elem.get('name'));
             ido.add(elem.get('ido'));
             keido.add(elem.get('keido'));
-            idokeido.add(elem.get('idokeido'));
-            //pos = elem.get('idokeido');
+            kubun.add(elem.get('kubun'));
+            namae.add(elem.get('namae'));
+            zyouhou1.add(elem.get('zyouhou1'));
+            zyouhou2.add(elem.get('zyouhou2'));
+            zyouhou3.add(elem.get('zyouhou3'));
+            zyuusyo.add(elem.get('zyuusyo'));
           });
-          return GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: _initialPosition,
-              zoom: 17,
-            ),
-            onMapCreated: (GoogleMapController controller) {
-              _controller = controller;
-            },
-            markers: documentList
-              .map((documents) => Marker(
-                markerId: MarkerId(documents['name']),
-                position: LatLng(documents['ido'],documents['keido']),
-              ))
-              .toSet(),
-            onTap: (LatLng latLang) {
-              print('Clicked: $latLang');
-            },
-            myLocationEnabled: true,
-          );
+          return gm;
         },
       ),
     );
   }
 
+
+
   Future<void> initialize() async {
     final snapshot =
     await FirebaseFirestore.instance.collection('toire').get();
     documentList = snapshot.docs;
-    GeoPoint pos_con = const GeoPoint(0.0, 0.0);
-
-    // LocationPermission permission = await Geolocator.checkPermission();
-    // if (permission == LocationPermission.denied) {
-    //   await Geolocator.requestPermission();
 
       print(
           "##################################################### initialize()");
       documentList.forEach((elem) {
-        print(elem.get('name'));
         print(elem.get('ido'));
         print(elem.get('keido'));
-        pos_con = elem.get('idokeido');
-        print(pos_con.latitude.toString() + ',' + pos.longitude.toString());
+        print(elem.get('kubun'));
+        print(elem.get('namae'));
+        print(elem.get('zyouhou1'));
+        print(elem.get('zyouhou2'));
+        print(elem.get('zyouhou3'));
+        print(elem.get('zyuusyo'));
       });
       print(
           "##################################################### initialize()");
+      initialize2();
     }
+
+  Future<void> initialize2() async {
+    gm = await GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: _initialPosition,
+        zoom: 17,
+      ),
+      onMapCreated: (GoogleMapController controller) {
+        _controller = controller;
+      },
+      markers: documentList
+          .map((documents) => Marker(
+        markerId: MarkerId(documents['namae']),
+        position: LatLng(documents['ido'],documents['keido']),
+        onTap: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            isDismissible: true,
+            context: context,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+            ),
+
+            builder: (BuildContext context) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    title: Text(documents['namae']),
+                  ),
+                  ListTile(
+                    title: Text(documents['zyuusyo']),
+                  ),
+                  ListTile(
+                    title: Text(documents['zyouhou1']),
+                  ),
+                  ListTile(
+                    title: Text(documents['zyouhou2']),
+                  ),
+                  ListTile(
+                    title: Text(documents['zyouhou3']),
+                  ),
+                ],
+
+              );
+            },
+          );
+        },
+      ))
+          .toSet(),
+      myLocationEnabled: true,
+    );
   }
-//}
+
+
+
+  // Future<void> initialize3() async {
+  //   //現在地の許可
+  //   LocationPermission permission = await Geolocator.checkPermission();
+  //   if (permission == LocationPermission.denied) {
+  //     await Geolocator.requestPermission();
+  //   }
+  // }
+
+}
