5a4a8acb6666954cef57da0d3a4ef9720c28a958
narir <si19048@edu.aomori-u.ac.jp>
2022-12-07 16:38:53 +0900

    Mapアプリ マーカーの表示範囲を制限、ルートの距離を表示、ルート解除ボタン（API削除）
    43,68~69,106~113,322~348,357　マーカーの表示範囲制限
    79~80,240~284 距離の表示、ルート解除ボタンの変更
    210~215 「メニュー閉じる」テキストボタン
    473=475 マップの+-ボタンと外部アプリの経路案内削除、マップの最小ズームと最大ズームの制限

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  //Firestoreから取得したList
   List<DocumentSnapshot> documentList = [];
+  //documentListから範囲制限をかけて取得したList
+  List<DocumentSnapshot> DLR = [];
 
   //List<BitmapDescriptor> colorList = [];
 
@@ -60,8 +62,11 @@ class _MyHomePageState extends State<MyHomePage> {
   late LatLng _initialPosition;
   late bool _loading;
   late GoogleMap gm;
-  //GoogleMap?  gm;
   late Divider line;
+
+  //表示範囲制限用のLatlng
+  late LatLng restriction1;
+  late LatLng restriction2;
   //late BitmapDescriptor markercolor;
 
   //String markerkubun = "";
@@ -69,8 +74,10 @@ class _MyHomePageState extends State<MyHomePage> {
   //route変数
   List<LatLng> points = [];
   MapsRoutes route = new MapsRoutes();
+  //push時にAPIは削除！！
   String googleApiKey = '';
-
+  String totalDistance = '距離';
+  DistanceCalculator distanceCalculator = new DistanceCalculator();
 
   GeoPoint pos = const GeoPoint(0.0, 0.0);
 
@@ -78,6 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
     accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
     distanceFilter: 100,
   );
+
 
 
   @override
@@ -94,10 +102,18 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _initialPosition = LatLng(position.latitude, position.longitude);
       _loading = false;
+
+      restriction1 = LatLng(
+          _initialPosition.latitude + 0.0090133729745762,
+          _initialPosition.longitude + 0.010966404715491394
+      );
+      restriction2 = LatLng(
+          _initialPosition.latitude - 0.0090133729745762,
+          _initialPosition.longitude - 0.010966404715491394
+      );
       print(position);
     });
   }
-
 
   //現在位置を更新し続ける
   void positionstrem() {
@@ -111,8 +127,6 @@ class _MyHomePageState extends State<MyHomePage> {
               .toString()}');
         });
   }
-
-
 
   // void _markercolor(){
   //   documentList.forEach((elem) {
@@ -132,10 +146,6 @@ class _MyHomePageState extends State<MyHomePage> {
   //   }
   // }
 
-
-
-
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -144,53 +154,68 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       endDrawer: Drawer(
         width: 210,
-        child: ListView(
-          children:  [
-            Container(
-              height: 35,
-              width: double.infinity,
-              color: Colors.blueAccent,
-              alignment: Alignment.center,
-              child:  Text("メニュー一覧",style: TextStyle(color:Colors.white, fontSize: 16)),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView(
+                  children:  [
+                    Container(
+                      height: 35,
+                      width: double.infinity,
+                      color: Colors.blueAccent,
+                      alignment: Alignment.center,
+                      child:  Text("メニュー一覧",style: TextStyle(
+                          color:Colors.white,
+                          fontSize: 16
+                        )
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("アプリの使い方"),
+                      onTap: () {
+
+                      },
+                    ),
+                    ListTile(
+                      title: Text("トイレ情報修正案"),
+                      subtitle: Text("（Google Forms）"),
+                      onTap: () async {
+                        final url = Uri.parse(
+                          'https://docs.google.com/forms/d/e/1FAIpQLSczt2P7yPl4FXtT-qYfXxHPoC3Y5-mcmeffyrtmWByZRISCXA/viewform',
+                        );
+                        if (await canLaunchUrl(url)) {
+                          launchUrl(url);
+                        } else {
+                          print("Can't launch $url");
+                        }
+                      },
+                    ),
+                    ListTile(
+                      title: Text("アプリの評価"),
+                      subtitle: Text("（Google Forms）"),
+                      onTap: () async {
+                        final url = Uri.parse(
+                          'https://docs.google.com/forms/d/e/1FAIpQLScuUDQ81k8TPi2w15trgbMQeUhq_fFTafpPZZF86vfldDPt0g/viewform',
+                        );
+                        if (await canLaunchUrl(url)) {
+                          launchUrl(url);
+                        } else {
+                          print("Can't launch $url");
+                        }
+                      },
+                    ),
+                  ],
+                ),
             ),
-            ListTile(
-                title: Text("アプリの使い方"),
-                onTap: () {
-                  print('ontap');
-                },
-            ),
-            ListTile(
-              title: Text("トイレ情報修正案"),
-              subtitle: Text("（Google Forms）"),
-              onTap: () async {
-                  final url = Uri.parse(
-                    'https://docs.google.com/forms/d/e/1FAIpQLSczt2P7yPl4FXtT-qYfXxHPoC3Y5-mcmeffyrtmWByZRISCXA/viewform',
-                  );
-                  if (await canLaunchUrl(url)) {
-                    launchUrl(url);
-                  } else {
-                  // ignore: avoid_print
-                    print("Can't launch $url");
-                  }
-                },
-            ),
-            ListTile(
-              title: Text("アプリの評価"),
-              subtitle: Text("（Google Forms）"),
-              onTap: () async {
-                final url = Uri.parse(
-                  'https://docs.google.com/forms/d/e/1FAIpQLScuUDQ81k8TPi2w15trgbMQeUhq_fFTafpPZZF86vfldDPt0g/viewform',
-                );
-                if (await canLaunchUrl(url)) {
-                  launchUrl(url);
-                } else {
-                  // ignore: avoid_print
-                  print("Can't launch $url");
-                }
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
               },
+              child: Text('メニューを閉じる'),
             ),
           ],
         ),
+
       ),
       body: FutureBuilder(
         future: initialize1(),
@@ -207,26 +232,65 @@ class _MyHomePageState extends State<MyHomePage> {
             zyuusyo.add(elem.get('zyuusyo'));
             color.add(elem.get('color'));
           });
-
           return gm;
-
         },
       ),
 
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 9),
+            child: SizedBox(
+              width: 120,
+              height: 45,
+              child: FloatingActionButton.extended(
+                tooltip: 'Action!',
+                  label: Text(totalDistance, style: TextStyle(
+                      color:Colors.black38,
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  backgroundColor: Colors.white.withOpacity(0.9),
+                  onPressed: null,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 120,
+              height: 40,
+              child: FloatingActionButton.extended(
+                tooltip: 'Action!',
+                label: Text('ルート解除', style: TextStyle(
+                    color:Colors.white,
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+                backgroundColor: Colors.redAccent,
+                onPressed: () {
+                  //ルート、ルート用のList、距離の表示をクリア
+                  route.routes.clear();
+                  points.clear();
+                  totalDistance = '距離';
+                  setState(() {});
+                },
+              ),
+            ),
+        ],
+      ),
     );
   }
-
-
-
-
-
 
   Future<void> initialize1() async {
     final snapshot =
     await FirebaseFirestore.instance.collection('toire').get();
     documentList = snapshot.docs;
 
-      print("##################################################### initialize()");
+      print("##################################################### initialize1()");
       documentList.forEach((elem) {
         print(elem.get('ido'));
         print(elem.get('keido'));
@@ -239,7 +303,7 @@ class _MyHomePageState extends State<MyHomePage> {
         print(elem.get('zyuusyo'));
         print(elem.get('color'));
       });
-      print("##################################################### initialize()");
+      print("##################################################### initialize1()");
       initialize2();
     }
 
@@ -255,15 +319,42 @@ class _MyHomePageState extends State<MyHomePage> {
       endIndent: 10,
     );
 
+    //documentListから範囲制限をかけて取得
+    DLR = documentList.where((documents) {
+      if ((documents['ido'] < restriction1.latitude && documents['ido'] > restriction2.latitude)
+      && (documents['keido'] < restriction1.longitude && documents['keido'] > restriction2.longitude)) {
+        return true;
+      } else {
+        return false;
+      }
+    }).toList();
+    print('===========================================================');
+    print(restriction1);
+    print(restriction2);
+    print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM');
+    DLR.forEach((elem) {
+      print(elem.get('ido'));
+      print(elem.get('keido'));
+      print(elem.get('kubun'));
+      print(elem.get('namae'));
+      print(elem.get('zyouhou1'));
+      print(elem.get('zyouhou2'));
+      print(elem.get('zyouhou3'));
+      print(elem.get('zyouhou4'));
+      print(elem.get('zyuusyo'));
+      print(elem.get('color'));
+    });
+    print('===========================================================');
+
     gm = await GoogleMap(
       initialCameraPosition: CameraPosition(
         target: _initialPosition,
-        zoom: 17,
+        zoom: 15,
       ),
       onMapCreated: (GoogleMapController controller) {
         _controller = controller;
       },
-      markers: documentList
+      markers: DLR
           .map((documents) => Marker(
         markerId: MarkerId(documents['namae']),
         // icon: BitmapDescriptor.defaultMarkerWithHue(
@@ -271,7 +362,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // ),
         //icon: colorList,
         //icon: markercolor,
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
         //icon: markercolor,
         position: LatLng(documents['ido'],documents['keido']),
         onTap: () {
@@ -302,20 +393,10 @@ class _MyHomePageState extends State<MyHomePage> {
                             Color.fromRGBO(0,191,255, 1.0), googleApiKey,
                             travelMode: TravelModes.walking);
                       Navigator.of(context).pop();
-
-                      // Positioned(
-                      //   left: 10,
-                      //   bottom: 45,
-                      //   child: FloatingActionButton(
-                      //     child: Icon(Icons.logout),
-                      //     onPressed: () {
-                      //       route.routes.clear();
-                      //       setState(() {});
-                      //     },
-                      //   ),
-                      // );
-
-                      setState(() {});
+                      setState(() {
+                        totalDistance =
+                            distanceCalculator.calculateRouteDistance(points, decimals: 1);
+                      });
                     },
                     child: Text('ルート検索'),
                   ),
@@ -380,7 +461,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           // Text(documents['zyouhou4'],),
                         ],
                       ),
-
                 ],
               );
             },
@@ -390,17 +470,25 @@ class _MyHomePageState extends State<MyHomePage> {
           .toSet(),
       polylines: route.routes,
       myLocationEnabled: true,
+      mapToolbarEnabled: false,
+      zoomControlsEnabled: false,
+      minMaxZoomPreference: MinMaxZoomPreference(8, 19),
     );
   }
-
-
 
   Future<void> initialize3() async {
     //現在地の許可
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       await Geolocator.requestPermission();
+      //再描画
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (BuildContext context) => super.widget)
+      );
     }
+
   }
 
 }
